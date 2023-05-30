db267e6e06ed84493a3c4dc39299c89e703cdfc7
David Lukes <dafydd.lukes@gmail.com>
2023-05-31 01:40:07 +0200

    Clean up and blackify

--- anom_oral2013.py
+++ anom_oral2013.py
@@ -1,11 +1,8 @@
-#!/cnk/users/home/lukes/.linuxbrew/bin/python3
+#!/bin/env python3
 
 import os
 import sys
 import argparse
-# import multiprocessing as mp
-
-sys.path.append("/cnk/users/home/lukes/.local/lib/python3.4/site-packages")
 
 import re
 import numpy as np
@@ -13,10 +10,9 @@ from lxml import etree
 import scipy.io.wavfile as scwav
 
 
-class Transcript():
-    """A wrapper offering a shared interface to vertical and trs transcripts.
+class Transcript:
+    """A wrapper offering a shared interface to vertical and trs transcripts."""
 
-    """
     def __init__(self, doc_root, trs=False, id=None):
         self.root = doc_root
         self.trs = trs
@@ -57,9 +53,7 @@ class Transcript():
 
 
 def doc_generator(input, trs=False):
-    """Yield input vertical or trs one doc (as parsed XML) at a time.
-
-    """
+    """Yield input vertical or trs one doc (as parsed XML) at a time."""
     with open(input) as fh:
         if trs:
             root = etree.parse(input).getroot()
@@ -75,9 +69,8 @@ def doc_generator(input, trs=False):
 
 
 def anonymize(doc, wav_dir, sin_freq=440):
-    """Anonymize recording corresponding to doc id.
+    """Anonymize recording corresponding to doc id."""
 
-    """
     def time2samples(time_attrib, rate):
         time = float(time_attrib)
         return int(time * rate)
@@ -96,18 +89,16 @@ def anonymize(doc, wav_dir, sin_freq=440):
     dtype = samples.dtype
     channels = samples.shape[1]
     if channels > 1:
-        sys.stderr.write(
-            "{} is not mono ({} channels).\n".format(wav_file, channels))
+        sys.stderr.write("{} is not mono ({} channels).\n".format(wav_file, channels))
     for seg in doc.segs():
         if doc.is_anom_seg(seg):
             start = time2samples(doc.seg_start(seg), fs)
-            end = min(time2samples(doc.seg_end(seg), fs),
-                      len(samples) - 1)
-            peak = equiv_sine_peak(samples[start:end + 1])
+            end = min(time2samples(doc.seg_end(seg), fs), len(samples) - 1)
+            peak = equiv_sine_peak(samples[start : end + 1])
             sin = gen_sin(end + 1 - start, sin_freq, fs) * peak
             sin = sin.astype(dtype)
             for i in range(channels):
-                samples[start:end + 1, i] = sin
+                samples[start : end + 1, i] = sin
     return fs, samples
 
 
@@ -119,11 +110,13 @@ def process(doc, args):
         sys.stderr.write("Saved {}.\n".format(out_file))
         return 0
     else:
-        sys.stderr.write("Target file {} already exists in output "
-                         "directory {}. Please remove it or specify a "
-                         "different output dir.\n".format(
-                             os.path.basename(out_file),
-                             os.path.dirname(out_file)))
+        sys.stderr.write(
+            "Target file {} already exists in output "
+            "directory {}. Please remove it or specify a "
+            "different output dir.\n".format(
+                os.path.basename(out_file), os.path.dirname(out_file)
+            )
+        )
         sys.stderr.flush()
         return 1
 
@@ -131,27 +124,53 @@ def process(doc, args):
 def parse_invocation(argv):
     if argv is None:
         argv = sys.argv[1:]
-    parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(
+        description="""
 Anonymize spoken corpus recordings in WAV format based on
 timestamps in corpus vertical.
-""")
-    parser.add_argument("input", help="""
+"""
+    )
+    parser.add_argument(
+        "input",
+        help="""
 a spoken corpus with timestamps for each segment, in vertical format;
 alternatively, a .trs file (see option --trs)
-""")
-    parser.add_argument("-i", "--input-dir", help="""
+""",
+    )
+    parser.add_argument(
+        "-i",
+        "--input-dir",
+        help="""
 path to directory containing input WAV files
-""", required=True)
-    parser.add_argument("-o", "--output-dir", help="""
+""",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        help="""
 path to directory where output WAV files will be saved
-""", required=True)
-    parser.add_argument("-f", "--freq", help="""
+""",
+        required=True,
+    )
+    parser.add_argument(
+        "-f",
+        "--freq",
+        help="""
 frequency of sine wave (in Hz) to replace anonymized segments; default
 is 440 Hz
-""", type=int, default=440)
-    parser.add_argument("-t", "--trs", action="store_true", help="""
+""",
+        type=int,
+        default=440,
+    )
+    parser.add_argument(
+        "-t",
+        "--trs",
+        action="store_true",
+        help="""
 consider input to be .trs instead of vertical
-""")
+""",
+    )
     return parser.parse_args(argv)
 
 
@@ -160,13 +179,10 @@ def main(argv=None):
     results = []
     for doc in doc_generator(args.input, args.trs):
         results.append(process(doc, args))
-    # pool = mp.Pool(4)
-    # results = [pool.apply_async(process, args = (doc,))
-    #            for doc in doc_generator(args.vertical)]
-    # results = [result.get() for result in results]
     if any(results):
-        sys.stderr.write("WARNING: Some already existing files were not"
-                         " overwritten.\n")
+        sys.stderr.write(
+            "WARNING: Some already existing files were not" " overwritten.\n"
+        )
         return 0
     else:
         return 0
